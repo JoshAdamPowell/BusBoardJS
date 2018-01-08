62ac9863158bec7e7c1876c575a05cda0e4498bd
Benjamin Dobson <Benjamin.Dobson@softwire.com>
2018-01-08 11:10:53 +0000

    nearly working server!

--- ArrivalTimes.js
+++ ArrivalTimes.js
@@ -9,18 +9,22 @@ function getArrivalTimes(stopID){
 	const arrivalsPromise = request.get('https://api.tfl.gov.uk/StopPoint/' + stopID + '/Arrivals')
 
 
-	arrivalsPromise.then((val) => processJSON(val));
+	return arrivalsPromise.then((val) => processJSON(val));
 
 	function processJSON(json) {
 		var arrivalsData = JSON.parse(json)
 		arrivalsData.sort(function(a,b) { return a.timeToStation - b.timeToStation })
 		arrivalsData.length = 5
-		arrivalsData.forEach(arrival => console.log(stringForArrival(arrival)))
+		var newArrivalsData = []
+		arrivalsData.forEach(arrival => newArrivalsData.push(simplifiedArrivalObject(arrival)))
+		return newArrivalsData
 	}
 
-	function stringForArrival(arrival) {
-		const str =
-			`${arrival.lineName}	${arrival.destinationName}	${Math.floor(arrival.timeToStation/60)}m ${arrival.timeToStation % 60}s`
-		return str
+	function simplifiedArrivalObject(arrival) {
+		return {
+			lineName: arrival.lineName,
+			destination: arrival.destinationName,
+			timeToArrival: arrival.timeToStation
+		}
 	}
 }
--- index.js
+++ index.js
@@ -3,7 +3,50 @@ const request = require('request-promise-native')
 const arrivalTimes = require ('./ArrivalTimes')
 const postcodeLookup = require ('./PostcodeLookup')
 const busStopFinder = require ('./BusStopFinder')
+const express = require('express')
 
-postcodeLookup.locationOfPostcode("NW5 1TL").then(val => busStopFinder.busStopsByLocation(...val).then(val => console.log(val)))
 
-arrivalTimes.getArrivalTimes("490008660N")
+// Start the server
+const app = express()
+
+// Listen for connections on /departureBoard
+app.get('/departureBoard', departureBoard)
+
+app.listen(3000)
+
+function departureBoard(request, result) {
+	const postcode = request.query.postcode
+	const busStopsPromise = busStopsByPostcode(postcode)
+	const outputPromise = busStopsPromise.then(arrivalsDataFromBusStops)
+	outputPromise.then((output) => {
+		result.send(output)
+	})
+}
+
+function arrivalsDataFromBusStops(busStops) {
+	var arrivalTimesPromises = [];
+	for (const busStop of busStops) {
+		const arrivalTimesPromise = arrivalTimes.getArrivalTimes(busStop).then(arrivalTimes => {
+			console.log(arrivalTimes)
+			return {
+				id: busStop,
+				arrivals: arrivalTimes
+			}
+		})
+		arrivalTimesPromises.push(arrivalTimesPromise)
+	}
+	const combinedArrivalsPromise = Promise.all(arrivalTimesPromises)
+	return combinedArrivalsPromise.then((results) => {
+		const stringFromResults = JSON.stringify(results)
+		return stringFromResults
+	})
+}
+
+function busStopsByPostcode(postcode) {
+	const locationPromise = postcodeLookup.locationOfPostcode(postcode)
+	return locationPromise.then(latlong => busStopFinder.busStopsByLocation(...latlong))
+}
+
+// postcodeLookup.locationOfPostcode("NW5 1TL").then(val => busStopFinder.busStopsByLocation(...val).then(val => console.log(val)))
+//
+// arrivalTimes.getArrivalTimes("490008660N")
